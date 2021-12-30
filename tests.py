95164aed098574a9e9c1f4a66581f5fb3c056d29
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2021-12-29 16:28:56 -0800

    Pylint cleanups

--- tests.py
+++ tests.py
@@ -40,30 +40,31 @@ class TestAWBWReplay(unittest.TestCase):
                 pass
 
 class TestAWBWGameState(unittest.TestCase):
+    """Tests for the AWBWGame* classes"""
 
     def test_short_replay(self):
         """Test a basic short replay."""
         example_replay = os.path.join(TEST_REPLAYS_DIR, "short_replay.zip")
         with AWBWReplay(example_replay) as replay:
             states = [AWBWGameState(replay_initial=replay.game_info())]
-            
+
             for action in replay.actions():
                 states.append(states[-1].apply_action(AWBWGameAction(action)))
 
             assert len(replay.turns()) == states[-1].game_info["turn"] + 1
-            assert all([len(state.players) == 2 for state in states])
+            assert all((len(state.players) == 2 for state in states))
 
     def test_basic_replay(self):
         """Test a medium length replay."""
         example_replay = os.path.join(TEST_REPLAYS_DIR, "basic_replay.zip")
         with AWBWReplay(example_replay) as replay:
             states = [AWBWGameState(replay_initial=replay.game_info())]
-            
+
             for action in replay.actions():
                 states.append(states[-1].apply_action(AWBWGameAction(action)))
 
             assert len(replay.turns()) == states[-1].game_info["turn"] + 1
-            assert all([len(state.players) == 2 for state in states])
+            assert all((len(state.players) == 2 for state in states))
 
 if __name__ == "__main__":
     unittest.main()
